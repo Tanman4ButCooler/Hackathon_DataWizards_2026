d0af32dfd777c72b0c9fba673e51f82222e9e62a
Tanman4ButCooler <107323433+Tanman4ButCooler@users.noreply.github.com>
2026-02-20 15:09:36 -0700

    Bottem row and map size fix

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const button = document.createElement("nav-btn active");
+document.addEventListener("DOMContentLoaded", function () {
 
-button.addEventListener('click', () => {
-    console.log('Hi');
-    
+    //Buttons 
+    const todayBtn = document.querySelector(".nav-btn.active");
+    const weekBtn = document.querySelector(".date-btn");
+    const monthBt = document.querySelector(".date-btn");
+    const accidentList = document.querySelector(".nav-btn");
+    const heatMapBtn = document.querySelector(".nav-btn");
+    const statBtn = document.querySelector(".nav-btn");
+    const reportBtn = document.querySelector(".nav-btn");
+    const abtBtn = document.querySelector(".nav-btn");
+
+    //Button functions
+    if (todayBtn) {
+        todayBtn.addEventListener("click", function () {
+            alert("Today button clicked!");
+        });
+    }
+    //About button tells us about the developers of this program
+    if (weekBtn) {
+        nav-btn.addEventListener("click", function () {
+            alert("Sudev: Aspiring Pilot\n");
+         });
+    }
+
+
 });
