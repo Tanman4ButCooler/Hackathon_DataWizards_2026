1593ec31829cf58a57e266ad361d2684c2188bed
rythem253 <miolavoro9@gmail.com>
2026-02-20 20:32:51 -0700

    Made Today and About  Us Btn

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     //Buttons 
-    const todayBtn = document.querySelector(".nav-btn.active");
-    const weekBtn = document.querySelector(".date-btn");
-    const monthBt = document.querySelector(".date-btn");
-    const accidentList = document.querySelector(".nav-btn");
-    const heatMapBtn = document.querySelector(".nav-btn");
-    const statBtn = document.querySelector(".nav-btn");
-    const reportBtn = document.querySelector(".nav-btn");
-    const abtBtn = document.querySelector(".nav-btn");
+    const todayBtn = document.querySelector(".todayBtn");
+    const weekBtn = document.querySelector(".weekBtn");
+    const monthBt = document.querySelector(".monthBtn");
+    const accidentList = document.querySelector(".accidentsListBtn");
+    const heatMapBtn = document.querySelector(".heatMapBtn");
+    const statBtn = document.querySelector(".statBtn");
+    const reportBtn = document.querySelector(".reportBtn");
+    const abtBtn = document.querySelector(".aboutBtn");
 
     //Button functions
     if (todayBtn) {
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
     //About button tells us about the developers of this program
     if (abtBtn) {
         abtBtn.addEventListener("click", function () {
-            alert("Sudev: Aspiring Pilot\n");
+            alert("Sudev: Aspiring Pilot\nMeet: Aspiring Software Engineer\nRythem:Aspiring Software Developer\nTanveer:Aspiring Game Developer");
          });
     }
 });
